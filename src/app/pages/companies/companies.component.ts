f6a068b1d000aa4e3a2d42829f068d3772da0440
unknown <nicoladc92@gmail.com>
2025-04-01 09:36:14 +0200

    SNG-003 Creado menu lateral y rutado
    -Añadido menu lateral responsivo para diferentes tamaños de pantalla
    -Añadidos iconos de font-awesome por cdn

---
+++ src/app/pages/companies/companies.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-companies',
+  imports: [TranslateModule],
+  templateUrl: './companies.component.html',
+  styleUrl: './companies.component.scss',
+})
+export class CompaniesComponent {}
