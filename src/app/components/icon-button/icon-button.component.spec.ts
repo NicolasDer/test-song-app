b9b42e746b731ddd779fee055e69caee6d397b66
unknown <nicoladc92@gmail.com>
2025-04-04 13:25:42 +0200

    SNG-010 Añadido test unitario para el componente de Icono

--- src/app/components/icon-button/icon-button.component.spec.ts
+++ src/app/components/icon-button/icon-button.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { IconButtonComponent } from './icon-button.component';
+import { By } from '@angular/platform-browser';
 
 describe('IconButtonComponent', () => {
   let component: IconButtonComponent;
@@ -8,7 +9,7 @@ describe('IconButtonComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [IconButtonComponent]
+      imports: [ IconButtonComponent ]
     })
     .compileComponents();
 
@@ -17,7 +18,37 @@ describe('IconButtonComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('Deberia crearse', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Deberia asignar los valores de entrada correctamente', async () => {
+    component.iconClass = 'test-icon';
+    component.type = 'submit';
+    fixture.detectChanges(); 
+    
+    const buttonElement = fixture.debugElement.query(By.css('button'));  
+    expect(buttonElement.nativeElement.type).toBe('submit');
+
+    const iconElement = fixture.debugElement.query(By.css('button i'));
+    expect(iconElement.nativeElement.classList).toContain('test-icon');
+  });
+
+  it('Debería emitir buttonClick al clickarlo', () => {
+    spyOn(component.buttonClick, 'emit'); 
+    
+    const buttonElement = fixture.debugElement.query(By.css('button')); 
+    buttonElement.triggerEventHandler('click', null);
+    
+    expect(component.buttonClick.emit).toHaveBeenCalled(); 
+  });
+
+  it('Debería llamar a onClick al clickarlo', () => {
+    spyOn(component, 'onClick'); 
+
+    const buttonElement = fixture.debugElement.query(By.css('button'));
+    buttonElement.triggerEventHandler('click', null); 
+
+    expect(component.onClick).toHaveBeenCalled(); 
   });
 });
