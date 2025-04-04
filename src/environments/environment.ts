5a16bde65022a4592796e997451e8a694477e320
unknown <nicoladc92@gmail.com>
2025-04-04 14:55:05 +0200

    SNG-012 Añadida documentacion del proyecto

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: false,
     apiUrl: 'http://localhost:3000',
     appTitle: 'My Songs App - Development',
-    apiErrors: false,
-    apiDelay: 0
+    apiErrors: true, //Controla si el guardado y eliminacion funcionan o no para probar el flujo de aplicacion
+    apiDelay: 2000 // Delay en  ms que tendran las llamadas a json-server
   };
